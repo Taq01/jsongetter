2864cac3966ca70aeda440163be680f77f33568c
Taq01 <mrxsaq@gmail.com>
2025-05-06 18:00:20 +0300

    Revert "for v1.0.0 release"
    This reverts commit 34c93bb4e30251acb9d8f64be47134f36d28695a.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="jsongetter",
     version="1.0.0",  
-    packages=["jsongetter"],
+    packages=find_packages(),
     description="A library for dynamic search and retrieve data from JSON datasets",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
